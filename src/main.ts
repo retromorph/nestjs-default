950be431dd44e35f2ae92065df58d3658a0ae37c
retromorph <musalov2@gmail.com>
2022-08-09 03:23:46 +0300

    add security and compression

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import compression from '@fastify/compress';
+import helmet from '@fastify/helmet';
+import fastifyCsrf from 'fastify-csrf';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      cors: true,
+    },
+  );
+  await app.register(compression, { encodings: ['gzip'] });
+  await app.register(helmet);
+  await app.register(fastifyCsrf);
+
   await app.listen(3000);
 }
 
